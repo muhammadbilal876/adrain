f69af7c23a7121122d20449cb3112ee7b5080447
muhammadbilal876 <158255435+muhammadbilal876@users.noreply.github.com>
2025-07-03 01:51:43 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors({
-  origin: 'https://adrain-driver-app.web.app',
+  origin: 'https://adrain-driver.web.app',
   origin: 'https://shopping-cart-4.web.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
